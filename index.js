e9705804c5110f6b370111c6ee5101d154a16f8b
Lily Whitford <lilywhitford2@gmail.com>
2024-02-11 04:13:11 +0000

    linted code

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 
 // Create a new task by adding to the arrays
 // A new task will be created as incomplete
-function newTask(title, description) {
+const newTask = function(title, description) {
   const task = {
     title: title,
     description: description,
     complete: false,
 
-// Print the state of a task to the console in a nice readable way
-logState: function() {
-  console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
-},
+    // Print the state of a task to the console in a nice readable way
+    logState: function() {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
 
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-markCompleted: function () {
-  this.complete = true;
+    // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
+    markCompleted: function() {
+      this.complete = true;
     }
   };
-return task;
-}
+  return task;
+};
 
 
 // DRIVER CODE BELOW
 const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
-const task2 = newTask("Do Laundry", "😨");
-const tasks = [task1, task2];
+// const task2 = newTask("Do Laundry", "😨");
+// const tasks = [task1, task2];
 
 
 // newTask("Clean Cat Litter"); // task 0
